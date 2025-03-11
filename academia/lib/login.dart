efca20619727b2e0d8e77aeed82da6050e29dcaf
devtheozzinho <devtheozzinho@gmail.com>
2025-03-11 20:10:47 -0300

    coisas do login

---
+++ academia/lib/login.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter/widgets.dart';
+
+// class Login extends StatefulWidget{
+//   @override
+//   StatefulElement createElement() {
+//      super.createElement();
+//   }
+// }
