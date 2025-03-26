46c2b8600b3fb5640c90064fd0e8abe351d7d151
devtheozzinho <devtheozzinho@gmail.com>
2025-03-25 21:23:29 -0300

    implementação da appBar

--- academia/lib/main.dart
+++ academia/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './login/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MeuAplicativo(),
     );
   }
 }
