1b18ca764ead60db092dd57dcba3d8fe1f7b2613
devtheozzinho <devtheozzinho@gmail.com>
2025-04-22 21:09:44 -0300

    Adicionado novos detalhes na tela e feito algumas melhorias na tela

--- academia/lib/cards/page.dart
+++ academia/lib/cards/page.dart
@@ -29,74 +29,108 @@ class _CardProductState extends State<CardProduct> {
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           title: Text(
-            '',
-            style: GoogleFonts.poppins(
-              fontSize: 32,
-            ),
+            'Modelos',
+            style: GoogleFonts.poppins(fontSize: 32, color: Colors.black),
           ),
           centerTitle: true,
           foregroundColor: Colors.black,
           backgroundColor: Colors.transparent,
         ),
-        body: Stack(
-          children: [
-            GradientApp(
-              child: Card(
-                elevation: 8,
-                margin: EdgeInsets.all(25),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+        body: GradientApp(
+          child: Card(
+            elevation: 8,
+            margin: EdgeInsets.all(24),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ListTile(
+                  title: Text('Chevrolet Onix 2020 vermelho'),
+                  subtitle: Text('Categoria B - Compacto Com Ar'),
+                  titleTextStyle:
+                      GoogleFonts.poppins(fontSize: 18, color: Colors.black),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(15),
+                SizedBox(
+                  height: 45,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    'https://st4.depositphotos.com/21932260/23647/i/1600/depositphotos_236476052-stock-illustration-car-white-background.jpg',
+                    alignment: Alignment.center,
+                    height: 150,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 18,
                       ),
-                      child: Image.network(
-                        'https://st4.depositphotos.com/21932260/23647/i/1600/depositphotos_236476052-stock-illustration-car-white-background.jpg',
-                        alignment: Alignment.center,
-                        height: 150,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                      Row(
                         children: [
-                          Text(
-                            'Carro Modelo',
-                            style: GoogleFonts.poppins(
-                              fontSize: 18,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text('Vidro elétrico'),
+                                SizedBox(height: 4),
+                                Text('12BC4EF'),
+                                SizedBox(height: 4),
+                                Text('4 pessoas'),
+                                SizedBox(height: 4),
+                                Text('4 portas')
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'Descrição do carro e informações que vão ter',
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.black,
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text('ABS'),
+                                SizedBox(height: 4),
+                                Text('Automático'),
+                                SizedBox(height: 4),
+                                Text('Trava elétrica'),
+                                SizedBox(height: 4),
+                                Text('Ar-condicionado')
+                              ],
                             ),
-                            onPressed: () {},
-                            child: Text('Reservar'),
                           )
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: Colors.white,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 24, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        icon: Icon(Icons.directions_car),
+                        label: Text('Reservar'),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
